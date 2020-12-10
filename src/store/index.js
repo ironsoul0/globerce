45b526281017fdd2b0a87ed2ac7e7a91805d7491
ironsoul0 <timka2609@gmail.com>
2020-12-11 00:48:24 +0600

    almost done

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import modalReducer from "./reducers/modalSlice";
+import orderReducer from "./reducers/orderSlice";
 
 export default configureStore({
   reducer: {
     modal: modalReducer,
+    order: orderReducer,
   },
 });
